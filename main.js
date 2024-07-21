a68e9908465544d4c731018f73c242b838607ef0
Susmita Maity <smaitysusmita2002@gmail.com>
2024-07-21 14:25:47 +0530

    Add files via upload
    portfolio

---
+++ main.js
@@ -0,0 +1,36 @@
+var typed= new Typed(".text",{
+    strings: ["Frontend Developer.","Web Developer.","Full Stack Developer.","Data Analyst."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function
+    (event) {
+    let circle = document.querySelectorAll('.circle');
+    circle.forEach(function (progress) {
+        let degree = 0;
+        var targetDegree = parseInt(progress.getAttribute
+            ('data-degree'));
+        let color = progress.getAttribute('data-color');
+        let number = progress.querySelector('.number');
+        var interval = setInterval(function () {
+            degree += 1;
+            if (degree > targetDegree) {
+                clearInterval(interval);
+                return;
+            }
+            progress.style.background = `conic-gradient(${color} ${degree}%, #222 0%)`;
+            number.innerHTML = degree + '<span>%</span>';
+            number.style.color = color;
+        }, 50)
+    })
+});
+
+
+
